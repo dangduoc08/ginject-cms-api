353956ba905451f84b62b1ddfd654294cd9a0d42
Đăng Được <duoc200894@gmail.com>
2025-12-18 00:50:29 +0700

    test1

--- cmd/app/calc/calc_test.go
+++ cmd/app/calc/calc_test.go
@@ -3,8 +3,8 @@ package calc
 import "testing"
 
 func TestAdd(t *testing.T) {
-	got := Add(1, 5)
-	want := 6
+	got := Add(1, 3)
+	want := 4
 
 	if got != want {
 		t.Errorf("expected %d, got %d", want, got)
